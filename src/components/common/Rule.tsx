90d1543bab7e9dd6883f09786f4bd1e2a74df632
AlexanderOI <vialexoriba1257@gmail.com>
2023-12-16 22:18:53 -0300

    refactor: new folder structure

--- src/components/common/Rule.tsx
+++
@@ -1,8 +0,0 @@
-export function Rule({ title, description }: { title: string, description: string }) {
-  return (
-    <div className="mb-3 pr-6">
-      <span className="text-indigo-500 font-bold">{title}:</span>
-      <p>{description}</p>
-    </div>
-  )
-}
--- src/utils/codeGenerator.ts
+++
@@ -1,9 +0,0 @@
-export const codeGenerator = () => {
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
-  let code = ''
-  for (let i = 0; i < 6; i++) {
-    const index = Math.floor(Math.random() * characters.length)
-    code += characters.charAt(index)
-  }
-  return code
-}
